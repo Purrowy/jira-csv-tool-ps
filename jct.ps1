30965ea0b803462d474b629ed7e6f2739ed2f705
Tomasz Borkowski <tomasz.borkowski@opuscapita.com>
2024-06-11 19:27:56 +0200

    downloads using PAT

--- jct.ps1
+++ jct.ps1
@@ -12,11 +12,11 @@ Set-Content -Path $fixed_source_csv -Value $fixed_content
 # Trying to import just one column thru pipeline doesn't work cause import-csv needs to run first anyway
 # Import-Csv $csvFile | select asdasd
 # This works, but number of columns has to be hardcoded. Might be easier to select them later, if they are being indexed by number
-$imported_csv = Import-Csv $fixed_source_csv -Header (1..6) | Select-Object -Skip 1
+$imported_csv = Import-Csv $fixed_source_csv -Header (1..36) | Select-Object -Skip 1
 
 # Prepares a list of attachments to download
 $target_list = @()
-$target_columns = $imported_csv | Select-Object -Property "1", "6"
+$target_columns = $imported_csv | Select-Object -Property "2", "36"
 
 foreach($row in $target_columns) {
     $target_list += $row
@@ -25,17 +25,22 @@ foreach($row in $target_columns) {
 Write-Host $target_list
 
 # Download listed attachments and save them under ticket's number
-$download_path = "$PSScriptRoot\work\"
+<# $download_path = "$PSScriptRoot\work\"
 foreach ($ticket in $target_list) {
-    $link = $ticket."6"
-    $filename = $ticket."1"
-
-    Write-Host $link
+    $link = $ticket."36"
+    $filename = $ticket."2"
+    $token = "xyz"
+    $output = "$download_path$filename"
+    # $curlCommand = "curl -H `"Authorization: Bearer $token`" -o $download_path$filename $link"
+    $headers = @{
+        Authorization = "Bearer $token"
+    }
 
      try {
-        Invoke-WebRequest -Uri $link -OutFile $download_path$filename
+        # Write-Host $curlCommand
+        Invoke-WebRequest -Uri $link -Headers $headers -Outfile $download_path$filename
         Write-Host "done"
     } catch {
         Write-Host "failed"
     }
-}
+} #>
